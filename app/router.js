0de9e5dfab4d4f7a44b5e241149b2c40a12f3de2
maoguijun <guijun.mao@loncus.com>
2018-05-14 12:02:13 +0800

    修改接口

--- app/router.js
+++ app/router.js
@@ -5,9 +5,9 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.post('/', controller.home.index);
-  router.get('/user/:id', controller.user.info);
-  router.post('/register', controller.register.index);
-  router.post('/login/account', controller.loginAccount.index);
+  router.post('/api/', controller.home.index);
+  router.get('/api/user/:id', controller.user.info);
+  router.post('/api/register', controller.register.index);
+  router.post('/api/login/account', controller.loginAccount.index);
   // router.post('/init', controller.init.index);
 };
