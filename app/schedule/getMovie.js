b67903c5a9d839cfe34c70e926417ffa3429255a
maoguijun <guijun.mao@loncus.com>
2018-05-17 14:16:06 +0800

    no message

--- app/schedule/getMovie.js
+++ app/schedule/getMovie.js
@@ -4,18 +4,10 @@ module.exports = {
   schedule: {
     // interval: '2 days', // 1 分钟间隔
     // interval: '10 s', // 1 分钟间隔
-    cron: '0 13 14 * * *',
+    cron: '0 18 14 * * *',
     type: 'all', // 指定所有的 worker 都需要执行
   },
   async task(ctx) {
-    // ctx.app.mysql.insert('movies', {
-    //   id: '123465',
-    //   detailUrl: 'aa',
-    //   imgUrl: 'aa',
-    //   title: 'title',
-    //   date: '2018-05-26',
-    //   createAt: new Date().getUTCMilliseconds(),
-    // });
     console.log('开始执行');
     start('https://javlog.com/cn', ctx, 1, 4);
   },
